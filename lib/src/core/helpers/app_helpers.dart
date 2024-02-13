ca70c3466910bcd398e256009fb260a94714a6ac
Cristofer Juarez <cjuarez@hypertech.mx>
2024-02-13 15:48:57 -0600

    update: dateToString method added

--- lib/src/core/helpers/app_helpers.dart
+++ lib/src/core/helpers/app_helpers.dart
@@ -4,4 +4,14 @@ abstract class AppHelpers {
   static Future<bool> launchUrl(String url) async {
     return await launchUrlString(url);
   }
+
+  static String dateToString(DateTime dateTime) {
+    final String day =
+        dateTime.day < 10 ? '0${dateTime.day}' : '${dateTime.day}';
+    final String month =
+        dateTime.month < 10 ? '0${dateTime.month}' : '${dateTime.month}';
+    final String year = '${dateTime.year}';
+
+    return '$day/$month/$year';
+  }
 }

52f11f60cc3cb50fc0bc48eeb12dd050b1495283
Cristofer Juarez <cjuarez@hypertech.mx>
2024-02-13 11:55:41 -0600

    feature: app theme added

--- lib/src/ui/theme/app_theme.dart
+++ lib/src/ui/theme/app_theme.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'app_colors.dart';
 
 abstract class AppTheme {
-  static ThemeData themeData() {
-    return ThemeData(
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      useMaterial3: true,
+  static ThemeData themeData({required bool isDark}) {
+    return (isDark)
+        ? ThemeData.dark().copyWith(
+            useMaterial3: true,
+            colorScheme: _colorScheme,
+            appBarTheme: _appBarTheme,
+            textTheme: _textTheme(isDark),
+          )
+        : ThemeData.light().copyWith(
+            useMaterial3: true,
+            colorScheme: _colorScheme,
+            appBarTheme: _appBarTheme,
+            textTheme: _textTheme(isDark),
+          );
+  }
+
+  static final _colorScheme =
+      ColorScheme.fromSeed(seedColor: AppColors.primary);
+  static const _appBarTheme = AppBarTheme(backgroundColor: AppColors.primary);
+
+  static TextTheme _textTheme(bool isDark) {
+    final color = (isDark) ? Colors.white : Colors.black;
+    return TextTheme(
+      titleLarge: GoogleFonts.cabin(color: color),
+      titleMedium: GoogleFonts.cabin(color: color),
+      titleSmall: GoogleFonts.cabin(color: color),
+      bodyLarge: GoogleFonts.cabin(color: color),
+      bodyMedium: GoogleFonts.cabin(color: color),
+      bodySmall: GoogleFonts.cabin(color: color),
+      labelLarge: GoogleFonts.cabin(color: color),
+      labelMedium: GoogleFonts.cabin(color: color),
+      labelSmall: GoogleFonts.cabin(color: color),
+      displayLarge: GoogleFonts.cabin(color: color),
+      displayMedium: GoogleFonts.cabin(color: color),
+      displaySmall: GoogleFonts.cabin(color: color),
+      headlineLarge: GoogleFonts.cabin(color: color),
+      headlineMedium: GoogleFonts.cabin(color: color),
+      headlineSmall: GoogleFonts.cabin(color: color),
     );
   }
 }

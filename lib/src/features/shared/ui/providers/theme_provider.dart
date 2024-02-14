17bc91459f0bb17906e13d8c61c993a36b483f83
Cristofer Juarez <criller.apps@gmail.com>
2024-02-13 22:32:57 -0600

    update: saving isDark on preferences

--- lib/src/features/shared/ui/providers/theme_provider.dart
+++ lib/src/features/shared/ui/providers/theme_provider.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:fulltimeforce_app/src/core/local_storage/app_preferences.dart';
 
 class ThemeProvider with ChangeNotifier {
   bool _isDark = false;
   bool get isDark => _isDark;
 
+  void init() {
+    _isDark = AppPreferences.getIsDarkMode();
+  }
+
   void toggleTheme({bool? value}) {
     (value != null) ? _isDark = value : _isDark = !_isDark;
+    AppPreferences.setIsDarkMode(_isDark);
     notifyListeners();
   }
 }

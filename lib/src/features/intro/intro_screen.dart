356c338fec4a6cb068bdac7ada56bb13d1055783
Cristofer Juarez <criller.apps@gmail.com>
2024-02-13 22:33:10 -0600

    feature: intro screen

--- lib/src/features/intro/intro_screen.dart
+++ lib/src/features/intro/intro_screen.dart
@@ -1,13 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:fulltimeforce_app/src/core/local_storage/app_preferences.dart';
+
+import '../../core/constants/app_assets.dart';
+import '../../ui/widgets/common/my_button.dart';
+import '../home/ui/screens/home_screen.dart';
 
 class IntroScreen extends StatelessWidget {
   const IntroScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [],
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+          vertical: kToolbarHeight,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(
+              AppAssets.welcome,
+              fit: BoxFit.cover,
+            ),
+            Text(
+              'Welcome to FullTimeForce',
+              style: Theme.of(context).textTheme.headlineLarge,
+              textAlign: TextAlign.center,
+            ),
+            const Spacer(),
+            MyButton(
+              text: 'Get Started',
+              onPressed: () {
+                AppPreferences.setFirstTime(false);
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (_) => const HomeScreen()),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }

f4f2f56c9b61247ac1fe92b1bfc45e296616ec7c
Cristofer Juarez <criller.apps@gmail.com>
2024-02-13 22:33:54 -0600

    refactor: open/closed principle on commit entity

--- lib/src/features/home/data/models/commit_model.dart
+++ lib/src/features/home/data/models/commit_model.dart
@@ -1,11 +1,28 @@
-// import '../../domain/entities/commit_entity.dart';
+import '../../domain/entities/commit.dart';
+import 'commit_author_model.dart';
+import 'tree_model.dart';
+import 'verification_model.dart';
 
-// class CommitModel extends CommitEntity {
-//   const CommitModel({required super.title, required super.authorID});
-
-//   factory CommitModel.fromJson(Map<String, dynamic> json) {
-//     return CommitModel(title: json['title'], authorID: json['authorID']);
-//   }
-
-//   Map<String, dynamic> toJson() => {'title': title, 'authorID': authorID};
-// }
+class CommitModel extends Commit {
+  const CommitModel({
+    required super.author,
+    required super.committer,
+    required super.message,
+    required super.tree,
+    required super.url,
+    required super.commentCount,
+    required super.verification,
+  });
+  //fromJson
+  factory CommitModel.fromJson(Map<String, dynamic> json) {
+    return CommitModel(
+      author: CommitAuthorModel.fromJson(json["author"]),
+      committer: CommitAuthorModel.fromJson(json["committer"]),
+      message: json["message"],
+      tree: TreeModel.fromJson(json["tree"]),
+      url: json["url"],
+      commentCount: json["comment_count"],
+      verification: VerificationModel.fromJson(json["verification"]),
+    );
+  }
+}

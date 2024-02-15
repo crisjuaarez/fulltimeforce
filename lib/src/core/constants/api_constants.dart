e91ed9bf25d441621d596ce1c363ca06a0934d28
Cristofer Juarez <criller.apps@gmail.com>
2024-02-15 13:27:30 -0600

    update: fine-grained token

--- lib/src/core/constants/api_constants.dart
+++ lib/src/core/constants/api_constants.dart
@@ -1,4 +1,5 @@
 class ApiConstants {
   static const baseUrl = 'https://api.github.com';
-  static const token = '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';
+  static const token =
+      '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';
 }

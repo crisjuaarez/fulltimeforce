15cca3e0e11cddbe44807441181b5df913d5d516
Cristofer Juarez <criller.apps@gmail.com>
2024-02-15 13:43:01 -0600

    feature: token from enviroment

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'src/core/local_storage/app_preferences.dart';
 import 'src/my_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   await AppPreferences.initialize();
   runApp(const MyApp());
 }
--- lib/src/core/constants/api_constants.dart
+++ lib/src/core/constants/api_constants.dart
@@ -1,5 +1,6 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class ApiConstants {
   static const baseUrl = 'https://api.github.com';
-  static const token =
-      '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';
+  static final token = dotenv.get('GITHUB_TOKEN', fallback: '');
 }
